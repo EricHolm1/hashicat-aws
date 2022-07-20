50ae7a1f4a602e1fae433f38f9a08e48ae3eb097
EricHolm1 <109673999+EricHolm1@users.noreply.github.com>
2022-07-20 15:32:59 -0400

    Update s3-bucket.tf

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,8 +2,7 @@ module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
   version = "2.8.0"
 
-  bucket_prefix = "holm-eric-1"
-  bucket = "my-s3-bucket"
+  bucket = "holm-eric-1"
   acl    = "private"
 
   versioning = {
